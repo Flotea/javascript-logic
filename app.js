005c1a3b0caf82fb2d9a4fd6951b6c88289ac993
Flotea <hgrimmer0786@myhta.org>
2017-03-17 14:11:38 -1000

    solved first

--- app.js
+++ app.js
@@ -9,69 +9,70 @@
 */
 
 // Addition (+)
-5 + 9;
+console.log(5 + 9);
+// 14
 
-"one" + "one";
+// "one" + "one";
 
-// Subtraction (-)
-8 - 20;
+// // Subtraction (-)
+// 8 - 20;
 
-// Multiplication (*)
-2 * 0;
+// // Multiplication (*)
+// 2 * 0;
 
-// Division (/)
-15/50;
+// // Division (/)
+// 15/50;
 
-// Modulus (%)
-5 % 2;
+// // Modulus (%)
+// 5 % 2;
 
-// Increment (++)
-2++;
+// // Increment (++)
+// 2++;
 
--1++;
+// -1++;
 
-// Decrement (--)
-5--;
+// // Decrement (--)
+// 5--;
 
-/*
-* Comparison Operators (These return a boolean value of true or false)
-* For those with the boolean answer false, change the expression to evaluate to true
-*/
+// /*
+// * Comparison Operators (These return a boolean value of true or false)
+// * For those with the boolean answer false, change the expression to evaluate to true
+// */
 
-// Strict Equal (===)
-5 === "five";
+// // Strict Equal (===)
+// 5 === "five";
 
-// Not Equal (!==)
-5!== "five";
+// // Not Equal (!==)
+// 5!== "five";
 
-// Greater Than (>)
-5 > 10;
+// // Greater Than (>)
+// 5 > 10;
 
-// Less Than (<)
-"20" < 20;
+// // Less Than (<)
+// "20" < 20;
 
-// Greater than or Equal to (>=)
-undefined >= null;
+// // Greater than or Equal to (>=)
+// undefined >= null;
 
-// Less than or Equal to (<=)
-null <= null; 
+// // Less than or Equal to (<=)
+// null <= null; 
 
-/*
-* Logical (or Relational) Operators
-*/
+// /*
+// * Logical (or Relational) Operators
+// */
 
-// And (&&) - If both the operands satisfy the statment, then the condition becomes true
-1 > 0 && 1 < 0;
+// // And (&&) - If both the operands satisfy the statment, then the condition becomes true
+// 1 > 0 && 1 < 0;
 
-// Or (||) - If any of the two operands satisfy the statement, then the condition becomes true.
-2 > 0 || 2 > 20;
+// // Or (||) - If any of the two operands satisfy the statement, then the condition becomes true.
+// 2 > 0 || 2 > 20;
 
-// Not (!) - Reverses the logical state of its operand.
-!true;
+// // Not (!) - Reverses the logical state of its operand.
+// !true;
 
-/*
-* Conditionals
-* Using the variables given to you, create if, else, and else if statements
-*/
-var a = "apple";
-var b = 2;
+// /*
+// * Conditionals
+// * Using the variables given to you, create if, else, and else if statements
+// */
+// var a = "apple";
+// var b = 2;
